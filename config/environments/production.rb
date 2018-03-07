4251aac579384461e73b3737cf97e0af8e700936
kanghy22 <nasmurf1hk@gmail.com>
2018-03-07 02:10:02 -0500

    fixes contd pt3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Default URL for mailer/Devise
-  config.action_mailer.default_url_options = { host: 'pacific-crag-74003.herokuapp.com' }
+
 end
