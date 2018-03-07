bcf05bc441e981d329f47f2ffecad1e572d08134
kanghy22 <nasmurf1hk@gmail.com>
2018-03-06 23:49:39 -0500

    schema error fix 002

--- db/schema.rb
+++ db/schema.rb
@@ -29,11 +29,11 @@ ActiveRecord::Schema.define(version: 20170806174945) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.change :email, :string, null: false, default: "" 
+    t.change :email, :string, null: false, default: ""
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index [nil], name: "index_users_on_unlock_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
 end
