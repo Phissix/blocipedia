7a799da517ebcb0ad81226092d591b94f3e054fd
kanghy22 <nasmurf1hk@gmail.com>
2018-03-07 02:19:37 -0500

    heroku fixes contd002

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
+  # Default URL for mailer/Devise
+  config.action_mailer.default_url_options = { host: 'pacific-beyond-52982.herokuapp' }
 end
