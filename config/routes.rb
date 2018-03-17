742e85f1837300a43770944bfd852438834e8008
kanghy22 <nasmurf1hk@gmail.com>
2018-03-17 03:55:54 -0400

    implemented stripegem

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'charges/new'
 
   resources :wikis
+  resources :charges, only: [:new, :create, :destroy]
   devise_for :users, :controllers => { :registrations => 'users/registrations' }
   root 'welcome#index'
 end
